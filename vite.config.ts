369d9625a3ffad57e77b9e86789d1e7537ffd802
elvision <elvisiondragon@gmail.com>
2026-05-07 21:18:24 +0700

    chore: simplify build output by setting logLevel to warn

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  logLevel: 'warn',
   plugins: [react(), cloudflare()],
   resolve: {
     alias: {
